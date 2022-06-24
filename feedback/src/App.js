7f4910670b008a627618d517f0a17c59d53d0831
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-06-24 15:49:04 +0100

    Adding rating component. In the rating component we have an arra of input radio buttons to set the rating. Adding new uuid plugin so each new item as an id.

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
@@ -9,6 +10,11 @@ import FeedbackForm from './components/FeedbackForm';
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const addFeedback = (newFeedBack) => {
+    newFeedBack.id = uuidv4()
+    setFeedback([newFeedBack, ...feedback])
+  }
+
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
@@ -19,7 +25,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback}
           handleDelete={deleteFeedback} />
