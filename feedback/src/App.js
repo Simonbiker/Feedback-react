d96e49e4acf1fe21c7ae1200df7b1f60af930390
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-06-28 09:31:32 +0100

    Adding context folder and file. Wrapping the routes in FeedbackProvider

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -8,6 +8,7 @@ import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
 import About from './pages/About';
+import { FeedbackProvider } from './context/FeedbackContext'
 
 
 function App() {
@@ -25,25 +26,28 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <FeedbackForm handleAdd={addFeedback} />
-              <FeedbackStats feedback={feedback} />
-              <FeedbackList feedback={feedback}
-                handleDelete={deleteFeedback} />
-            </>
-          }>
-          </Route>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback}
+                  handleDelete={deleteFeedback} />
+              </>
+            }>
+            </Route>
 
-          <Route path='/about' element={<About />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
+            <Route path='/about' element={<About />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+
 
   );
 }
