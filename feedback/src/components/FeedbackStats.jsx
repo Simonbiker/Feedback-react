9de2fe97ea759560483bced445a8143d32563b7e
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-06-28 09:50:10 +0100

    Adding feedbackContext to stats and list and removing unwanted props.

--- feedback/src/components/FeedbackStats.jsx
+++ feedback/src/components/FeedbackStats.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+    const {feedback} = useContext(FeedbackContext)
     // Calculate rating avg
 
     let average = feedback.reduce((accumulator, currentValue) => {
@@ -20,8 +22,5 @@ function FeedbackStats({ feedback }) {
   )
 }
 
-FeedbackStats.prototype = {
-    feedback: PropTypes.array.isRequired,
-}
 
 export default FeedbackStats
